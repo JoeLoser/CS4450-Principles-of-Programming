3411b94d32af901b4d98deccf95fd497fd91706f
JoeLoser <joeloser93@gmail.com>
2014-09-14 21:03:31 -0500

    next two functions: digitval and twine work now.

--- homeworks/hw0/hw0_jrlmwc.hs
+++ homeworks/hw0/hw0_jrlmwc.hs
@@ -14,3 +14,16 @@ nextlet 'Z' = 'A'
 nextlet c
 	| isAlpha(c) = succ(c)
 	| otherwise = c
+
+-- converts a digit char (0-9) to its corresponding numerical value
+-- if arg isn't a digit characters, returns -1
+digitval :: Char -> Int
+digitval c
+	| isDigit(c) = digitToInt(c)
+	| otherwise  = -1
+
+-- accepts two functions (say, f and g) and "twines" them together into a
+-- function that accepts a single argument and constructs a tuple of type
+-- (b, c) from the result of applying the two functions to the argument
+twine :: (a -> b) -> (a -> c) -> a -> (b, c)
+twine f g a = (f a, g a)

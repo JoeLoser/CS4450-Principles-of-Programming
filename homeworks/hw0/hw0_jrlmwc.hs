cf691fe4abb9305e7bf03bc052161080ea5dcc7a
JoeLoser <joeloser93@gmail.com>
2014-09-14 20:48:28 -0500

    nextlet works for all cases

--- homeworks/hw0/hw0_jrlmwc.hs
+++ homeworks/hw0/hw0_jrlmwc.hs
@@ -4,18 +4,13 @@
 --smaller :: (Integer, Integer) -> Integer
 --smaller (x, y) = if x <= y then x else y
 
-import Prelude
 import Data.Char -- for nextlet
 
 -- takes letter of alphabet and returns the letter immediately after it (A is considered after Z)
 -- if argument is not a letter, returns the argument unchanged
 nextlet :: Char -> Char
-nextlet c = 
-	if(isAlpha(c)) 
-		then if (c == 'z')
-			then 'a'
-			else succ(c) 
-		 --then if (c == 'Z')
-			--then 'A'
-			--else succ(c)
-		else c
+nextlet 'z' = 'a'
+nextlet 'Z' = 'A'
+nextlet c
+	| isAlpha(c) = succ(c)
+	| otherwise = c
